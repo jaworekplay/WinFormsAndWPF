30bf9639dc11241221f93ea4cbd5a5b8364f95eb
Lukasz Banach <jaworekplay@gmail.com>
2019-07-24 23:14:07 +0100

    Apparently Migrate() does it all for us to have the db created and the migrations applied

--- DatabaseSetup/DBSetup/Program.cs
+++ DatabaseSetup/DBSetup/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Internal;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,11 +12,14 @@ namespace DBSetup
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            
             using (var db = new Models.CompanyContext())
             {
+                Console.WriteLine("Applying schemas");
+                db.Database.Migrate();
+                Console.WriteLine("Retrieving users from db");
                 var usersInDB = db.Users.Count();
-                Console.Write($"Users in database: {usersInDB}");
+                Console.WriteLine($"Users in database: {usersInDB}");
             }
             Console.ReadKey();
         }
